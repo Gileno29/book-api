8db6e9f8aa0426daf0dcb710eea302241a15885d
Gileno <gileno.cr.duarte@gmail.com>
2024-07-23 15:58:44 -0300

    feat: adicionando funcao para checkout de books

--- main.go
+++ main.go
@@ -62,6 +62,31 @@ func createBook(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
+func chockoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing ID query parameter"})
+		return
+	}
+
+	book, err := getBooksById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not found"})
+		return
+	}
+
+	if book.Quatity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Book not available"})
+		return
+	}
+
+	book.Quatity -= 1
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
 func main() {
 	//pega uma instlncia do GIN para manipular as rotas
 	router := gin.Default()
@@ -69,7 +94,7 @@ func main() {
 	router.GET("/books", getBooks)
 	router.POST("/books", createBook)
 	router.GET("/books/:id", bookById)
-
+	router.PATCH("/checkout", chockoutBook)
 	router.Run("localhost:8080")
 
 }
